d2eae931e74b900b07340262b1913c8e750e9adb
Denatle <iq.labozin@gmail.com>
2023-07-16 03:34:40 +0300

    raawr

---
+++ CrystalCannon/CC.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx; // requires BepInEx.dll and BepInEx.Harmony.dll
+using UnboundLib; // requires UnboundLib.dll
+using UnboundLib.Cards; // " "
+using HarmonyLib; // requires 0Harmony.dll
+using CC.Cards;
+using UnityEngine;
+
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable StringLiteralTypo
+
+
+namespace CC
+{
+    [BepInDependency("com.willis.rounds.unbound")] // necessary for most modding stuff here
+    [BepInDependency("pykess.rounds.plugins.playerjumppatch")] // fixes multiple jumps
+    [BepInDependency("pykess.rounds.plugins.legraycasterspatch")] // fixes physics for small players
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch")] // fixes allowMultiple and blacklistedCategories
+    [BepInDependency("pykess.rounds.plugins.gununblockablepatch")] // fixes gun.unblockable
+    [BepInDependency("pykess.rounds.plugins.temporarystatspatch")] // fixes Taste Of Blood, Pristine Perserverence, and Chase when combined with cards from PCE
+    [BepInDependency("pykess.rounds.plugins.moddingutils")] // utilities for cards and cardbars
+    [BepInDependency("com.dk.rounds.plugins.zerogpatch")]
+    [BepInPlugin(ModId, ModName, "0.0.1")]
+    [BepInProcess("Rounds.exe")]
+    public class CC : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            UnityEngine.Debug.Log("Crystal Cannon It WORKS!!!"); 
+            new Harmony(ModId).PatchAll();
+        }
+
+        private void Start()
+        {
+            instance = this;
+            // register credits with unbound
+            Unbound.RegisterCredits(ModName, new[] { "Denatle" }, null, "");
+
+            // build all cards
+            CustomCard.BuildCard<CrystalCannon>();
+        }
+
+        private const string ModId = "denatle.rounds.plugins.crystalcannon";
+        private const string ModName = "Crystal Cannon";
+        public const string ModInitials = "CC";
+    }
+}
---
+++ CrystalCannon/Cards/CrystalCannon.cs
@@ -0,0 +1,75 @@
+﻿using UnboundLib.Cards;
+using UnityEngine;
+
+// ReSharper disable ParameterHidesMember
+
+namespace CC.Cards;
+
+public class CrystalCannon : CustomCard
+{
+    public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats,
+        CharacterStatModifiers statModifiers, Block block)
+    {
+    }
+
+    public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health,
+        Gravity gravity, Block block, CharacterStatModifiers characterStats)
+    {
+        statModifiers.health = 0.00001f;
+        gun.percentageDamage = 50f;
+    }
+
+    public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health,
+        Gravity gravity, Block block, CharacterStatModifiers characterStats)
+    {
+    }
+
+    protected override string GetTitle()
+    {
+        return "Crystal Cannon";
+    }
+
+    protected override string GetDescription()
+    {
+        return "Get killed on any damage. Kill anyone with 2 shots.";
+    }
+
+    protected override GameObject? GetCardArt()
+    {
+        return null;
+    }
+
+    protected override CardInfo.Rarity GetRarity()
+    {
+        return CardInfo.Rarity.Rare;
+    }
+
+    protected override CardInfoStat[] GetStats()
+    {
+        return new[]
+        {
+            new CardInfoStat()
+            {
+                positive = true,
+                stat = "Damage",
+                simepleAmount = CardInfoStat.SimpleAmount.aHugeAmountOf
+            },
+            new CardInfoStat()
+            {
+                positive = false,
+                stat = "Health",
+                simepleAmount = CardInfoStat.SimpleAmount.aLotLower
+            }
+        };
+    }
+
+    protected override CardThemeColor.CardThemeColorType GetTheme()
+    {
+        return CardThemeColor.CardThemeColorType.EvilPurple;
+    }
+
+    public override string GetModName()
+    {
+        return CC.ModInitials;
+    }
+}
